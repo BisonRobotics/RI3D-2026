26c9b026e0770198a90dea14660a832a8ccc0bce
dylanrattai <dylan.rattai@ndus.edu>
2026-01-12 20:09:58 -0600

    Update climb subsystem to 1 motor with soft limits. Limit encoder values TBD.

--- src/main/java/frc/robot/commands/ClimbCommand.java
+++ src/main/java/frc/robot/commands/ClimbCommand.java
@@ -1,38 +1,55 @@
-/*package frc.robot.commands;
+package frc.robot.commands;
 
-import static frc.robot.Constants.ClimbConstants.kClimbSpeed;
+import static frc.robot.Constants.ClimbConstants.kClimbLowerLimit;
+import static frc.robot.Constants.ClimbConstants.kClimbUpperLimit;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ClimbSubsystem;
 
 public class ClimbCommand extends Command{
     ClimbSubsystem m_ClimbSubsystem;
-    int m_direction;
+    double m_speed;
 
-    public ClimbCommand(ClimbSubsystem climbSubsystem, int direction) {
+    public ClimbCommand(ClimbSubsystem climbSubsystem, double speed) {
         m_ClimbSubsystem = climbSubsystem;
-        m_direction = direction;
+        m_speed = speed;
 
         addRequirements(climbSubsystem);
     }
 
     @Override
     public void initialize() {
-        m_ClimbSubsystem.SetSpeed(m_direction * kClimbSpeed);
     }
 
+    // Stop climb on command end
     @Override
     public void end(boolean interrupted) {
         m_ClimbSubsystem.Stop();
     }
 
+    // Execute climb with encoder limits
     @Override
     public void execute() {
+        double pos = m_ClimbSubsystem.getPosition();
+
+        if (m_speed > 0.0 &&
+            pos >= kClimbUpperLimit) {
+            m_ClimbSubsystem.SetSpeed(0.0);
+            return;
+        }
+
+        if (m_speed < 0.0 &&
+            pos <= kClimbLowerLimit) {
+            m_ClimbSubsystem.SetSpeed(0.0);
+            return;
+        }
+
+        m_ClimbSubsystem.SetSpeed(m_speed);
     }
 
+    // Never finishes on its own
     @Override
     public boolean isFinished() {
         return super.isFinished();
     }
 }
-*/
--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -1,28 +1,21 @@
-/*package frc.robot.subsystems;
+package frc.robot.subsystems;
 
-import static frc.robot.Constants.ClimbConstants.kClimbLeaderPort;
-import static frc.robot.Constants.ClimbConstants.kClimbFollowerPort;
+import static frc.robot.Constants.ClimbConstants.kClimbMotorPort;
 import static frc.robot.Constants.ClimbConstants.kClimbStatorCurrentLimit;
 import static frc.robot.Constants.ClimbConstants.kClimbSupplyCurrentLimit;
 
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.Follower;
-import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
-import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimbSubsystem extends SubsystemBase {
-    private TalonFX climbLeader;
-    private TalonFX climbFollower;
+    private TalonFX climbMotor;
     private TalonFXConfiguration climbMotorConfig;
-    private VelocityTorqueCurrentFOC climbController;
-    private double targetRps = 0.0;
 
     public ClimbSubsystem() {
         // configure motor current limit with brake mode
@@ -41,30 +34,22 @@ public class ClimbSubsystem extends SubsystemBase {
             );
             
         // create motor and apply config
-        climbLeader = new TalonFX(kClimbLeaderPort);
-        climbLeader.getConfigurator().apply(climbMotorConfig);
-        climbFollower = new TalonFX(kClimbFollowerPort);
-        climbFollower.getConfigurator().apply(climbMotorConfig);
-        climbFollower.setControl(new Follower(kClimbLeaderPort, MotorAlignmentValue.Opposed));
-
-        // enable watchdog
-        climbLeader.setSafetyEnabled(true);
-        climbFollower.setSafetyEnabled(true);
+        climbMotor = new TalonFX(kClimbMotorPort);
+        climbMotor.getConfigurator().apply(climbMotorConfig);
     }
 
     public void Stop() {
-        targetRps = 0;
-        climbLeader.stopMotor();
+        climbMotor.stopMotor();
     }
 
     @Override
-    public void periodic() {
-        climbLeader.setControl(climbController.withVelocity(targetRps));
+    public void periodic() {}
+
+    public void SetSpeed(double speed) {
+        climbMotor.set(speed);
     }
 
-    public void SetSpeed(double rps) {
-        targetRps = rps;
-        climbLeader.setControl(climbController.withVelocity(targetRps));
+    public double getPosition() {
+        return climbMotor.getPosition().getValueAsDouble();
     }
 }
-*/
